df72564b59690db7ec51ed7e8b35634c763c3964
Nox57 <yannickparel@gmail.com>
2023-09-18 15:05:39 +0200

    Employee Form first draft

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import EmployeeForm from './pages/EmployeeForm'
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <EmployeeForm />
             <Footer />
         </div>
     )
