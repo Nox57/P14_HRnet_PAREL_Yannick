78ae4c32dbc94c09f410501bc340ec6bb7f807d6
Nox57 <yannickparel@gmail.com>
2023-09-22 15:44:39 +0200

    React Router Dom added, Routes added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import EmployeeForm from './pages/EmployeeForm'
+import EmployeeList from './pages/EmployeeList'
+import Home from './pages/Home'
 
 function App() {
     return (
         <div className="App">
-            <Header />
-            <EmployeeForm />
-            <Footer />
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/employee/add" element={<EmployeeForm />} />
+                    <Route path="/employee/list" element={<EmployeeList />} />
+                </Routes>
+                <Footer />
+            </Router>
         </div>
     )
 }
