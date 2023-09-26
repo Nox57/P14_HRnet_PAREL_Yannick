4fd63eb607b7fefeef5923d20e6853d39d7a88bb
Nox57 <yannickparel@gmail.com>
2023-09-26 13:02:51 +0200

    First draft with react-select to display dropdown

--- src/pages/EmployeeForm.js
+++ src/pages/EmployeeForm.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Select from 'react-select'
 import { US_states } from '../datas/US_states'
 import './EmployeeForm.css'
 
@@ -11,16 +12,21 @@ export default function EmployeeForm() {
         department: 'Sales', // default
         street: '',
         city: '',
-        state: US_states[0].abbreviation, // default
+        state: US_states[0].abbreviation, // Juste l'abréviation
         zipCode: '',
     })
 
-    const handleChange = (event) => {
-        const { name, value } = event.target
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }))
+    const departments = [
+        { value: 'Sales', label: 'Sales' },
+        { value: 'Marketing', label: 'Marketing' },
+        { value: 'Engineering', label: 'Engineering' },
+        { value: 'Human Resources', label: 'Human Resources' },
+        { value: 'Legal', label: 'Legal' },
+    ]
+
+    const handleChange = (name, option) => {
+        const newValue = option.value || option.target.value
+        setFormData((prevData) => ({ ...prevData, [name]: newValue }))
     }
 
     const saveEmployee = () => {
@@ -77,20 +83,20 @@ export default function EmployeeForm() {
                     value={formData.city}
                     onChange={handleChange}
                 />
-                <select
+                <Select
                     name="state"
-                    value={formData.state}
-                    onChange={handleChange}
-                >
-                    {US_states.map((state) => (
-                        <option
-                            key={state.abbreviation}
-                            value={state.abbreviation}
-                        >
-                            {state.name}
-                        </option>
-                    ))}
-                </select>
+                    value={{
+                        value: formData.state,
+                        label: US_states.find(
+                            (state) => state.abbreviation === formData.state
+                        ).name,
+                    }}
+                    onChange={(option) => handleChange('state', option)}
+                    options={US_states.map((state) => ({
+                        value: state.abbreviation,
+                        label: state.name,
+                    }))}
+                />
                 <input
                     type="number"
                     name="zipCode"
@@ -98,17 +104,16 @@ export default function EmployeeForm() {
                     value={formData.zipCode}
                     onChange={handleChange}
                 />
-                <select
+                <Select
                     name="department"
-                    value={formData.department}
-                    onChange={handleChange}
-                >
-                    <option value="Sales">Sales</option>
-                    <option value="Marketing">Marketing</option>
-                    <option value="Engineering">Engineering</option>
-                    <option value="Human Resources">Human Resources</option>
-                    <option value="Legal">Legal</option>
-                </select>
+                    value={departments.find(
+                        (dept) => dept.value === formData.department
+                    )}
+                    onChange={(option) =>
+                        handleChange('department', option.value)
+                    }
+                    options={departments}
+                />
             </form>
             <button onClick={saveEmployee}>Save</button>
         </div>
