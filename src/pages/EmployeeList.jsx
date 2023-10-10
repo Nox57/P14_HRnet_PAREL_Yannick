daa3baeaa1cbde54ac1dcac1658386be5f8614c0
Nox57 <yannickparel@gmail.com>
2023-10-11 00:26:42 +0200

    Removing @tanstack/react-table, migrating to material-react-table

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -1,221 +1,79 @@
-import { useState, useEffect } from 'react'
+import { useMemo } from 'react'
 import { useSelector } from 'react-redux'
-import {
-    createColumnHelper,
-    flexRender,
-    getCoreRowModel,
-    getPaginationRowModel,
-    getSortedRowModel,
-    useReactTable,
-} from '@tanstack/react-table'
+import { MaterialReactTable } from 'material-react-table'
 import './EmployeeList.css'
-import { rankItem } from '@tanstack/match-sorter-utils'
-
-const columnHelper = createColumnHelper()
-
-const columns = [
-    columnHelper.accessor('firstName', {
-        header: 'First Name',
-        filterFn: 'fuzzy',
-    }),
-    columnHelper.accessor('lastName', { header: 'Last Name' }),
-    columnHelper.accessor('dateOfBirth', { header: 'Date of Birth' }),
-    columnHelper.accessor('startDate', { header: 'Start Date' }),
-    columnHelper.accessor('department', { header: 'Department' }),
-    columnHelper.accessor('street', { header: 'Street' }),
-    columnHelper.accessor('city', { header: 'City' }),
-    columnHelper.accessor('state', { header: 'State' }),
-    columnHelper.accessor('zipCode', { header: 'Zip Code' }),
-]
-
-const fuzzyFilter = (row, column, value, addMeta) => {
-    console.log('toto')
-    // Rank the item
-    const itemRank = rankItem(row.getValue(column), value)
-
-    // Store the ranking info
-    addMeta(itemRank)
-
-    // Return if the item should be filtered in/out
-    console.log(itemRank.passed)
-    return itemRank.passed
-}
 
 export default function EmployeeTable() {
     const employees = useSelector((state) => state.employee.employees)
-    const [sorting, setSorting] = useState([])
-    const [globalFilter, setGlobalFilter] = useState('')
 
-    const table = useReactTable({
-        data: employees,
-        columns,
-        filterFns: {
-            fuzzy: fuzzyFilter,
-        },
-        state: {
-            sorting,
-            globalFilter,
-        },
-        enableGlobalFilter: true,
-        onSortingChange: setSorting,
-        onGlobalFilterChange: setGlobalFilter,
-        globalFilterFn: fuzzyFilter,
-        getCoreRowModel: getCoreRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-    })
+    const columns = useMemo(
+        () => [
+            {
+                accessorKey: 'firstName',
+                header: 'First Name',
+                size: 'auto',
+            },
+            {
+                accessorKey: 'lastName',
+                header: 'Last Name',
+                size: 'auto',
+            },
+            {
+                accessorKey: 'dateOfBirth',
+                header: 'Date of Birth',
+                size: 'auto',
+            },
+            {
+                accessorKey: 'startDate',
+                header: 'Start Date',
+                size: 'auto',
+            },
+            {
+                accessorKey: 'department',
+                header: 'Department',
+                size: 'auto',
+            },
+            {
+                accessorKey: 'street',
+                header: 'Street',
+                size: 150,
+            },
+            {
+                accessorKey: 'city',
+                header: 'City',
+                size: 150,
+            },
+            {
+                accessorKey: 'state',
+                header: 'State',
+                size: 'auto',
+            },
+            {
+                accessorKey: 'zipCode',
+                header: 'Zip Code',
+                size: 150,
+            },
+        ],
+        []
+    )
 
     return (
         <div>
             <a href="/add-employee">Add an employee</a>
-            <DebouncedInput
-                value={globalFilter ?? ''}
-                onChange={(value) => setGlobalFilter(value)}
+            <MaterialReactTable
+                columns={columns}
+                data={employees}
+                enableFullScreenToggle={false}
+                enableDensityToggle={false}
+                initialState={{ density: 'compact' }}
+                enableHiding={false}
+                enableColumnFilters={false}
+                muiTablePaginationProps={{
+                    rowsPerPageOptions: [10, 25, 50, 100],
+                    showFirstButton: false,
+                    showLastButton: false,
+                }}
             />
-            <table>
-                <thead>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <tr key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => (
-                                <th key={header.id}>
-                                    {header.isPlaceholder ? null : (
-                                        <div
-                                            {...{
-                                                className:
-                                                    header.column.getCanSort()
-                                                        ? 'cursor-pointer select-none'
-                                                        : '',
-                                                onClick:
-                                                    header.column.getToggleSortingHandler(),
-                                            }}
-                                        >
-                                            {flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
-                                            {{
-                                                asc: ' 🔼',
-                                                desc: ' 🔽',
-                                            }[header.column.getIsSorted()] ??
-                                                null}
-                                        </div>
-                                    )}
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody>
-                    {table.getRowModel().rows.map((row) => (
-                        <tr key={row.id}>
-                            {row.getVisibleCells().map((cell) => (
-                                <td key={cell.id}>
-                                    {flexRender(
-                                        cell.column.columnDef.cell,
-                                        cell.getContext()
-                                    )}
-                                </td>
-                            ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <div className="pagination-controls flex items-center gap-2">
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.setPageIndex(0)}
-                    disabled={!table.getCanPreviousPage()}
-                >
-                    {'<<'}
-                </button>
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.previousPage()}
-                    disabled={!table.getCanPreviousPage()}
-                >
-                    {'<'}
-                </button>
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.nextPage()}
-                    disabled={!table.getCanNextPage()}
-                >
-                    {'>'}
-                </button>
-                <button
-                    className="border rounded p-1"
-                    onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-                    disabled={!table.getCanNextPage()}
-                >
-                    {'>>'}
-                </button>
-                <span className="flex items-center gap-1">
-                    <div>Page</div>
-                    <strong>
-                        {table.getState().pagination.pageIndex + 1} of{' '}
-                        {table.getPageCount()}
-                    </strong>
-                </span>
-                <div className="select-container">
-                    <span className="flex items-center gap-1">
-                        Go to page:
-                        <input
-                            type="number"
-                            defaultValue={
-                                table.getState().pagination.pageIndex + 1
-                            }
-                            onChange={(e) => {
-                                const page = e.target.value
-                                    ? Number(e.target.value) - 1
-                                    : 0
-                                table.setPageIndex(page)
-                            }}
-                            className="border p-1 rounded w-16"
-                        />
-                    </span>
-                    <select
-                        value={table.getState().pagination.pageSize}
-                        onChange={(e) => {
-                            table.setPageSize(Number(e.target.value))
-                        }}
-                    >
-                        {[10, 20, 30, 40, 50].map((pageSize) => (
-                            <option key={pageSize} value={pageSize}>
-                                Show {pageSize}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-            </div>
         </div>
-    )
-}
-
-function DebouncedInput({
-    value: initialValue,
-    onChange,
-    debounce = 500,
-    ...props
-}) {
-    const [value, setValue] = useState(initialValue)
-
-    useEffect(() => {
-        setValue(initialValue)
-    }, [initialValue])
-
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            onChange(value)
-        }, debounce)
-
-        return () => clearTimeout(timeout)
-    }, [value])
-
-    return (
-        <input
-            {...props}
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-        />
     )
 }
